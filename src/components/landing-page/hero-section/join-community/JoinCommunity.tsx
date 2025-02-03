0c8785434a46e0b2bc0244b647838ad1a0965769
AbdulHameedi <sanilinity507@gmail.com>
2025-02-04 00:14:56 +0100

    update:blog

--- src/components/landing-page/hero-section/join-community/JoinCommunity.tsx
+++ src/components/landing-page/hero-section/join-community/JoinCommunity.tsx
@@ -2,15 +2,15 @@ import { ArrowDown, GetInTouch } from "../../../../assets/svg";
 
 const JoinCommunity = () => {
   return (
-    <div data-aos="fade-up" className="py-28">
-      <h1 className="text-5xl font-bold rotate-[2.64deg] mb-14 pl-40">
+    <div data-aos="fade-up" className="py-11 sm:py-28">
+      <h1 className="text-3xl sm:text-5xl font-bold rotate-[2.64deg] mb-14 md:pl-40 text-center md:text-left">
         Get in touch with us
       </h1>
-      <div className="flex gap-14 items-center ">
+      <div className="flex gap-14 items-center flex-col md:flex-row">
         <img src="./join-community.svg" alt="JoinUs" />
         <div className="relative">
           <img src={GetInTouch} alt="JoinCommunity"/>
-          <img src={ArrowDown} alt="ArrowDown" className="absolute top-[-13.5rem] left-[40px]" />
+          <img src={ArrowDown} alt="ArrowDown" className="absolute top-[-13.5rem] left-[40px] hidden md:block" />
         </div>
       </div>
     </div>
